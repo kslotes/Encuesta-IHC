e952852ed5b368dc88c79589a9fdb31d568863d0
kslotes <44619076+kslotes@users.noreply.github.com>
2021-03-20 12:50:30 -0300

    Nuevo menu de accesibilidad

--- main.js
+++ main.js
@@ -1,18 +1,91 @@
+let daltActivo = false;
+let disActivo = false;
+
+
+let checkDaltonismo = () => {
+    if (daltActivo) {
+        invDaltonismo();
+        document.getElementById("btn-Daltonismo").innerHTML = "Activar Daltonismo";
+    } else {
+        aplicarDaltonismo();
+        document.getElementById("btn-Daltonismo").innerHTML = "Desactivar Daltonismo";
+    }
+};
+let checkDislexia = () => {
+    if (disActivo) {
+        document.getElementById("btn-Dislexia").innerHTML = "Activar Dislexia";
+        invDislexia();
+    } else {
+        document.getElementById("btn-Dislexia").innerHTML = "Desactivar Dislexia";
+        aplicarDislexia();
+    }
+};
+
+let checkInit = () => {
+    if (daltActivo) {
+        document.getElementById("btn-Daltonismo").innerHTML = "Desactivar Daltonismo";
+    } else {
+        document.getElementById("btn-Daltonismo").innerHTML = "Activar Daltonismo";
+    }
+    if (disActivo) {
+        document.getElementById("btn-Dislexia").innerHTML = "Desactivar Dislexia";
+    } else {
+        document.getElementById("btn-Dislexia").innerHTML = "Activar Dislexia";
+    }
+};
+
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+        checkInit();
+    },
+    false
+);
+
 const aplicarDislexia = () => {
+    disActivo = true;
     let body = document.querySelector("body");
-    (body.style.fontFamily = "opendislexic"), Times, serif, "Arial";
+    (body.style.fontFamily = "opendislexic"), "Arial";
+    console.log("Aplico dislexia");
 };
 const aplicarDaltonismo = () => {
+    daltActivo = true;
     let section = document.querySelectorAll(".seccion-container");
     let comentarios = document.querySelector("#comentarios");
     let body = document.querySelector("body");
     let inputEdad = document.querySelector("#input-edad");
     inputEdad.style.backgroundColor = "#FF8039";
-    body.style.backgroundColor = "#cf9139";
+    body.style.background = "linear-gradient(to bottom, #cf8539 0%, rgba(233, 160, 65, 1) 100%)";
     comentarios.style.backgroundColor = "#f0ce9e";
-    section.forEach(el => {
+    section.forEach((el) => {
         el.style.backgroundColor = "#f0ce9e";
-    })
+    });
+    console.log("Aplico daltonismo");
+};
+
+const invDaltonismo = () => {
+    daltActivo = false;
+    let section = document.querySelectorAll(".seccion-container");
+    let comentarios = document.querySelector("#comentarios");
+    let body = document.querySelector("body");
+    let inputEdad = document.querySelector("#input-edad");
+    body.style.background = "linear-gradient(to bottom, rgba(233, 191, 124, 0.9) 0%, rgba(233, 160, 65, 0.5) 100%)";
+    inputEdad.style.backgroundColor = "rgba(233, 191, 124, 0.9)";
+    comentarios.style.backgroundColor = "#e6b570";
+    section.forEach((el) => {
+        el.style.backgroundColor = "#e6b570";
+    });
+    console.log("Invierto daltonismo ");
+};
+
+const invDislexia = () => {
+    disActivo = false;
+    let body = document.querySelector("body");
+    (body.style.fontFamily = "Roboto"), "Arial";
+    dislexiaActiva = false;
+    console.log("Invierto dislexia");
+    let invDislexia = document.getElementById("btn-invDislexia");
+    invDislexia.style.display = "none";
 };
 
 if (bowser.name.toLowerCase() !== "chrome") {
@@ -23,6 +96,7 @@ if (bowser.name.toLowerCase() !== "chrome") {
         confirmButtonText: `Entendido`,
     });
 }
+
 (async() => {
     const { value: disc } = await Swal.fire({
         title: `¿Tienes una discapacidad visual?`,
@@ -55,20 +129,17 @@ if (bowser.name.toLowerCase() !== "chrome") {
             inputValue: 2,
             confirmButtonText: "¡Listo!",
             showCancelButton: true,
-            cancelButtonText: "Cancelar"
+            cancelButtonText: "Cancelar",
         }).then((result) => {
             if (result.isConfirmed) {
-
-                if (document.getElementById('checkBoxDaltonismo').checked) {
+                if (document.getElementById("checkBoxDaltonismo").checked) {
                     aplicarDaltonismo();
-                    console.log('Aplico daltonismo')
                 }
-                if (document.getElementById('checkBoxDislexia').checked) {
+                if (document.getElementById("checkBoxDislexia").checked) {
                     aplicarDislexia();
-                    console.log('Aplico dislexia')
-
                 }
             }
-        })
+            checkInit();
+        });
     }
 })();
